e658e5d5836ccb18a141125d51f713fc0ee7cae3
ncrown1190 <ncrown88@outlook.com>
2022-12-22 13:08:24 -0500

    test image some

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -25,7 +25,10 @@ export default function Main() {
   //   ];
 
   const slides = [
-    { url: "http://localhost:3000/images/image-1.jpg", title: "beach" },
+    {
+      url: "http://localhost:3000/src/components/images/image-1.jpg",
+      title: "beach",
+    },
     { url: "./images/image-2.jpg", title: "boat" },
     { url: "./images/image-3.jpg", title: "forest" },
     { url: "./images/image-4.jpg", title: "city" },
